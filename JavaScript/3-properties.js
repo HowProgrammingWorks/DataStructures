33035791f96fcc62c0c10da123ebaadd4b317f46
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-02-01 06:42:20 +0200

    Improve example

--- JavaScript/3-properties.js
+++ JavaScript/3-properties.js
@@ -19,34 +19,38 @@ for (const key in person) {
 const name = 'Marcus Aurelius';
 const city = 'Rome';
 
-// Old style
-const person1 = { name: name, city: city };
-
-// New style
-const person2 = { name, city };
-
-// Dynamic field name
-const fieldName = 'city';
-const fieldValue = 'Roma';
-const person3 = {
-  name: 'Marcus Aurelius',
-  [fieldName]: fieldValue,
-};
-
-// Expression in field name
-const person4 = {
-  name: 'Marcus Aurelius',
-  ['city' + 'Born']: fieldValue,
-};
-
-// Function in field name
-function fn(s) {
-  return s + 'Born';
+{
+  const person = { name, city };
+  console.dir({ person });
 }
 
-const person5 = {
-  name: 'Marcus Aurelius',
-  [fn('city')]: fieldValue,
-};
+// Dynamic field name
+{
+  const fieldName = 'city';
+  const fieldValue = 'Roma';
+  const person = {
+    name: 'Marcus Aurelius',
+    [fieldName]: fieldValue,
+  };
+  console.dir({ person });
+}
 
-console.dir({ person1, person2, person3, person4, person5 });
+// Expression in field name
+{
+  const prefix = 'city';
+  const person = {
+    name: 'Marcus Aurelius',
+    [prefix + 'Born']: 'Roma',
+  };
+  console.dir({ person });
+}
+
+// Function in field name
+{
+  const fn = s => s + 'Born';
+  const person = {
+    name: 'Marcus Aurelius',
+    [fn('city')]: 'Roma',
+  };
+  console.dir({ person });
+}
