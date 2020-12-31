88002013e0b00145276869555ac8e0cf0b4d04ef
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-01-01 01:41:28 +0200

    Use toString instead of + '' to convert types

--- JavaScript/5-serialization.js
+++ JavaScript/5-serialization.js
@@ -15,6 +15,6 @@ console.dir(obj);
 console.log('person === obj is ', person === obj);
 
 const letters = ['A', 'B', 'C', 'D'];
-console.log(letters + '');
+console.log(letters.toString());
 console.log(JSON.stringify(letters));
 console.log(letters.join('---'));
